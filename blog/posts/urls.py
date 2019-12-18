ca9c3181a6d9d098a539d5b7a9912ff301c2234a
sid <milosida@gmail.com>
2019-12-18 15:06:35 +0300

    add app_name to posts

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 
+app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:post_id>', views.detail, name='detail'),
