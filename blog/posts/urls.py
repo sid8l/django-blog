ccb1b02b8223c293b5691dd11d38ff2bb5b582e3
sid <milosida@gmail.com>
2019-12-22 18:14:53 +0300

    add bootstrap

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'posts'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>', views.DetailView.as_view(), name='detail'),
-    path('<int:post_id>/add_comment', views.add_comment, name='add_comment'),
+    path('posts/<int:pk>', views.DetailView.as_view(), name='detail'),
+    path('posts/<int:post_id>/add_comment', views.add_comment, name='add_comment'),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -11,10 +11,11 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Post.objects.order_by('-pub_date')
-# def index(request):
-#     latest_post_list = Post.objects.order_by('-pub_date')[:5]
-#     context = {'latest_post_list': latest_post_list}
-#     return render(request, 'posts/index.html', context)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Все посты'
+        return context
 
 
 class DetailView(generic.DetailView):
@@ -22,15 +23,11 @@ class DetailView(generic.DetailView):
     template_name = 'posts/detail.html'
     context_object_name = 'post'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object.title
+        return context
 
-# def detail(request, post_id):
-#     post = get_object_or_404(Post, pk=post_id)
-#     ordered_comments = post.comment_set.order_by('-pub_date')
-#     context = {
-#         'post': post,
-#         'ordered_comments': ordered_comments,
-#     }
-#     return render(request, 'posts/detail.html', context)
 
 
 def add_comment(request, post_id):
