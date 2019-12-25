59ddd87282c3e5310c226bc2a0cf6c4eff456d7f
Novikov Artem <milosida@gmail.com>
2019-12-25 19:35:45 +0300

    change login ulr path

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.17.117.215']
 
 
 # Application definition
